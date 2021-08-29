be90422633edc0e344a5b1202f0cc66f641d10f2
Mohanamoorthi <praneshthanesh@gmail.com>
2021-08-29 15:42:43 +0530

    master

---
+++ src/org/javascenario/Javaq4.java
@@ -0,0 +1,17 @@
+package org.javascenario;
+
+public class Javaq4 {
+	public static void main(String[] args) {
+		
+	int a=15 ,b=47,c;
+	
+		c=a+b;
+		
+		if(c%2==0) {
+			System.out.println("The given number are even");
+		}
+		else {
+			System.out.println("The given number are odd");
+		}
+	}
+}
